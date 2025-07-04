101bc5bddb92302d19169d8719a949ced60ada2d
Guilherme-Vilani <gui.vilani79@gmail.com>
2025-07-04 19:16:38 -0400

    ultima alteracao antes da aula de sexta antes de introduzirmos a conexao com a api

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -8,66 +8,172 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  bool cardAberto = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Stack(
-            children: [
-              Image.asset("fundo_dashboard.png"),
-              Padding(
-                padding: const EdgeInsets.only(right: 12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      "teste3.png",
-                      width: 150,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.search_outlined,
-                          color: Colors.white,
-                        ),
-                        Icon(
-                          Icons.message_outlined,
-                          color: Colors.white,
-                        ),
-                        Icon(
-                          Icons.notifications_outlined,
-                          color: Colors.white,
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 145,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+          Container(
+            height: cardAberto
+                ? MediaQuery.of(context).size.height * 0.50
+                : MediaQuery.of(context).size.height * 0.36,
+            child: Stack(
+              children: [
+                Image.asset("fundo_dashboard.png"),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        "Olá, Guilherme",
-                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      Image.asset(
+                        "teste3.png",
+                        width: 150,
                       ),
-                      Text(
-                        "Ag 4317 Cc 014578-9",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.search_outlined,
+                            color: Colors.white,
+                          ),
+                          Icon(
+                            Icons.message_outlined,
+                            color: Colors.white,
+                          ),
+                          Icon(
+                            Icons.notifications_outlined,
+                            color: Colors.white,
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 ),
-              )
-            ],
+                Positioned(
+                  top: 145,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Olá, Guilherme",
+                          style: TextStyle(color: Colors.white, fontSize: 17),
+                        ),
+                        Text(
+                          "Ag 4317 Cc 014578-9",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.26,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 12, right: 12),
+                    child: Card(
+                      elevation: 10,
+                      color: Colors.white,
+                      child: Container(
+                        padding: EdgeInsets.all(12),
+                        width: MediaQuery.of(context).size.width * 0.93,
+                        height: cardAberto ? null : null,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.monetization_on_outlined,
+                                      color: Colors.red,
+                                      size: 30,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      "Saldo disponivel",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      cardAberto = !cardAberto;
+                                    });
+                                  },
+                                  icon: cardAberto
+                                      ? Icon(
+                                          Icons.keyboard_arrow_up,
+                                          size: 30,
+                                        )
+                                      : Icon(
+                                          Icons.keyboard_arrow_down,
+                                          size: 30,
+                                        ),
+                                )
+                              ],
+                            ),
+                            cardAberto
+                                ? Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "R\$ 10.000.000,00",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      ),
+                                      Text(
+                                        "Saldo + Limite: 143,00",
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                      Text(
+                                        "Entenda seu limite",
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                          fontSize: 16,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                      ),
+                                      Divider(),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            "Ver extrato",
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.red,
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  )
+                                : Container()
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
