e6135088cfebbcb9521e40a841a52544d6dd3d26
Guilherme-Vilani <gui.vilani79@gmail.com>
2025-07-16 21:52:44 -0400

    tela do pix feita

--- lib/views/pix_1.dart
+++ lib/views/pix_1.dart
@@ -8,11 +8,32 @@ class Pix1 extends StatefulWidget {
 }
 
 class _Pix1State extends State<Pix1> {
+  TextEditingController controllerPix = TextEditingController();
+
+  List cardVoceTambemPodeUsar = [
+    {
+      "icon": Icons.account_balance_outlined,
+      "title": "Agência\ne conta",
+      "route": ""
+    },
+    {"icon": Icons.content_copy_outlined, "title": "Pix\ne cola", "route": ""},
+    {"icon": Icons.qr_code_2_outlined, "title": "Código\nQR", "route": ""},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
         title: Text(
           "Pix",
           style: TextStyle(
@@ -38,18 +59,151 @@ class _Pix1State extends State<Pix1> {
             Container(
               padding: EdgeInsets.all(5),
               decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                  ),
-                  borderRadius: BorderRadius.circular(5)),
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
               child: TextField(
                 decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Nome, Chave ou Pix copia e cola"),
+                  border: InputBorder.none,
+                  hintText: "Nome, Chave ou Pix copia e cola",
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    controllerPix.text = value;
+                  });
+                },
               ),
             ),
-            SizedBox(height: 5,),
-            Text("Celular, CPF/CNPJ, e-mail, chave aleatória...")
+            SizedBox(
+              height: 5,
+            ),
+            Text("Celular, CPF/CNPJ, e-mail, chave aleatória..."),
+            controllerPix.text.isNotEmpty
+                ? GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.only(top: 18),
+                      alignment: Alignment.center,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 236, 9, 0),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      width: double.maxFinite,
+                      child: Text(
+                        "Continuar",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+                  )
+                : Container(),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              "Você também pode usar",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 1,
+              height: 130,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: cardVoceTambemPodeUsar.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        // Navigator.of(context).push(
+                        //   MaterialPageRoute(
+                        //     builder: (context) => cardVoceTambemPodeUsar[index]['route'],
+                        //   ),
+                        // );
+                      },
+                      child: Card(
+                        child: Container(
+                          width: 130,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(cardVoceTambemPodeUsar[index]['icon']),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                cardVoceTambemPodeUsar[index]['title'],
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              "Gerenciar",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.key_outlined),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Minhas Chaves",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text("Cadastre e gerencie suas chaves Pix")
+                                ],
+                              ),
+                            ],
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                          )
+                        ],
+                      ),
+                    ),
+                    Divider()
+                  ],
+                );
+              },
+            )
           ],
         ),
       ),
